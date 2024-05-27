fa1f9255dead2051c0d1674b256c8c53b3996080
Allan-Victor <akibuuka50@gmail.com>
2024-05-27 14:13:37 +0300

    project initialisation

---
+++ bvp-ms/src/main/java/com/breezevillepark_ms/bvp_ms/BvpMsApplication.java
@@ -0,0 +1,13 @@
+package com.breezevillepark_ms.bvp_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BvpMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BvpMsApplication.class, args);
+	}
+
+}
---
+++ bvp-ms/src/test/java/com/breezevillepark_ms/bvp_ms/BvpMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.breezevillepark_ms.bvp_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BvpMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
